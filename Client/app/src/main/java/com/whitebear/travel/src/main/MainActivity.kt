720ce2cd6901369ff23dbff64569f9ee67c8a892
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-03-28 22:20:39 +0900

    init client

--- Client/app/src/main/java/com/whitebear/travel/src/main/MainActivity.kt
+++ Client/app/src/main/java/com/whitebear/travel/src/main/MainActivity.kt
@@ -3,10 +3,16 @@ package com.whitebear.travel.src.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.whitebear.travel.R
+import com.whitebear.travel.config.BaseActivity
+import com.whitebear.travel.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+    fun initNavigation(){
+
     }
 }
